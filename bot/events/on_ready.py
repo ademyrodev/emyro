a0c8a78260d44c665063617449da1193f45c8405
ademyrodev <ademerebaixadp@gmail.com>
2024-03-01 21:02:11 +0100

    setup timer

--- bot/events/on_ready.py
+++ bot/events/on_ready.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from bot.logger import Logger
 from bot.event import Event
 
@@ -5,4 +7,14 @@ class OnReadyEvent(Event):
     name = "on_ready"
 
     async def run(self):
-        Logger.info("Bot is running!")
+        Logger.info("Emyro's ready!")
+
+        asyncio.create_task(self.time_timer())
+
+    async def time_timer(self):
+        while True:
+            Logger.info("Timer task executed!")
+            await asyncio.sleep(10)
+
+            # temporary placeholder.
+            ...
