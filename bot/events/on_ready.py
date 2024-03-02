c07501f9165b8b2fc1d7d502f1dd0e2085746a26
ademyrodev <ademerebaixadp@gmail.com>
2024-03-02 12:31:29 +0100

    implement daytime flip

--- bot/events/on_ready.py
+++ bot/events/on_ready.py
@@ -13,18 +13,4 @@ class OnReadyEvent(Event):
     async def run(self):
         Logger.info("Emyro's ready!")
 
-        asyncio.create_task(self.time_timer())
-
-    async def time_timer(self):
-        while True:
-            World.daytime = not World.daytime
-            Logger.info("Set daytime to", World.daytime)
-
-            now = datetime.now()
-            World.last_daytime_flip = (now.minute, now.second)
-
-            Logger.info("Last daytime flip:", World.last_daytime_flip)
-
-            await asyncio.sleep(30)
-            # temporary placeholder.
-            ...
+        asyncio.create_task(World.flip_daytime())
--- bot/game/world.py
+++ bot/game/world.py
@@ -1,5 +1,24 @@
+import asyncio
+
 from datetime import datetime
+
+from bot.logger import Logger
 
 class World:
     daytime = False
-    last_daytime_flip = (0, 0)
+    last_daytime_flip = -1
+    day_length = 10
+    day_length_seconds = 600
+
+    @staticmethod
+    async def flip_daytime():
+        while True:
+            World.daytime = not World.daytime
+            Logger.info("Set daytime to", World.daytime)
+
+            now = datetime.now()
+            World.last_daytime_flip = now.minute
+
+            Logger.info("Last daytime flip:", World.last_daytime_flip)
+
+            await asyncio.sleep(World.day_length_seconds)
